585fd48666dcf433b2e1b7447c8d9a9314e86b25
killer@killer.com <killer@killer.com>
2015-07-28 21:48:14 +0800

    modified

--- ConsoleAppMain/Program.cs
+++ ConsoleAppMain/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using classLibSub;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace ConsoleAppMain
     {
         static void Main(string[] args)
         {
+            var m = new MathOps();
+            m.Add(1, 2);
         }
     }
 }
